28a1927a8281f7f344003323059fc7ed06250f3a
marcialwushu <marcialwushu@gmail.com>
2018-10-17 16:51:15 -0300

    modified files

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var router = express.Router();
+var multer = require('multer');
+var upload = multer({dest: './uploads'});
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
@@ -8,11 +10,27 @@ router.get('/', function(req, res, next) {
 
 
 router.get('/register', function(req, res, next) {
-  res.render('register');
+  res.render('register', {title:'Register'});
 });
 
 router.get('/login', function(req, res, next) {
-  res.render('login');
+  res.render('login', {title: 'Login'});
+});
+
+router.post('/register', upload.single('profileimage') ,function(req, res, next) {
+  var name = req.body.name;
+  var email = req.body.email;
+  var username = req.body.username;
+  var password = req.body.password        ;
+  var password2 = req.body.password2;
+
+  console.log(req.file);
+  if(req.file){
+    console.log('Uploading File...');
+  }else {
+    console.log('No File Uploaded...')
+  }
+
 });
 
 
